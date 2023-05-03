9362cca72dd547601e069ea979d81f93b91354ca
andresh01 <andres85684@outlook.com>
2023-05-02 20:35:47 -0500

    change mongoose schema for dont use _v in database

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const ProductSchema = mongoose.Schema({
     //id: {type: String, required: true},
-    name: {type: String, required: true},
-    description: {type: String, required: false},
-    availableUnits: {type: Number, required: false, default: 0},
-    price: {type: Number, required: true},
-    category: {type: String, required: true},
-})
+    name: { type: String, required: true },
+    description: { type: String, required: false },
+    availableUnits: { type: Number, required: false, default: 0 },
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+
+},
+    { versionKey: false } //no muestra el _v en la base de datos mongoDB
+)
 
 exports.Products = mongoose.model("products", ProductSchema);
